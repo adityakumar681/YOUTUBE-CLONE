2e163453cde76c331438a06e5bf6f3b40ef8d3fa
Aaditya Singh <aadityasingh@Aadityas-MacBook-Air.local>
2025-09-20 15:34:23 +0530

    Full project commit

---
+++ src/Pages/Video/Video.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Video.css'
+import { useParams } from 'react-router-dom' 
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import Recommended from '../../Components/Recommended/Recommended'
+
+
+const Video = () => {
+
+  const {videoId, categoryId} = useParams();
+  return (
+
+    <div className='play-container'>
+       <PlayVideo videoId={videoId}/>
+       <Recommended categoryId={categoryId}/>
+    </div>
+  )
+}
+
+export default Video
